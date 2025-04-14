2151b0619f9be1693f35d752e3559e730cf699fe
jdmwihia <jeremwihia@gmail.com>
2025-04-14 19:50:53 +0300

    Update changes

--- bookings/admin.py
+++ bookings/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Seat
 
 # Register your models here.
+@admin.register(Seat)
+class SeatAdmin(admin.ModelAdmin):
+    list_display = ('number', 'is_booked', 'booked_by')
+    list_filter = ('is_booked',)
+    fields = ('number', 'is_booked', 'booked_by')
+    readonly_fields = ()
